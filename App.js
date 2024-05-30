ef223dac851a0cc7fd411cb619e1a7142ea17d49
Juliane Reis Maia <140528801+julianereism@users.noreply.github.com>
2024-05-30 17:05:13 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
+
+const App = () => {
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieData, setMovieData] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permissão de localização não concedida', 'Por favor, conceda permissão de localização para obter a localização.');
+        return;
+      }
+
+      let locationData = await Location.getCurrentPositionAsync({});
+      setLocation(locationData);
+    })();
+  }, []);
+
+  const handleSearch = async () => {
+    if (movieTitle.trim() === '') {
+      Alert.alert('Aviso', 'Por favor, insira um título de filme válido.');
+      return;
+    }
+    try {
+      const apiKey = '3e1362f4'; 
+      const apiUrl = `https://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`;
+
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (data.Response === 'True') {
+        setMovieData(data);
+      } else {
+        Alert.alert('Erro', 'Filme não encontrado. Verifique o título e tente novamente.');
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro', 'Houve um problema na busca do filme. Tente novamente mais tarde.');
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#FDFDFD', padding: 20 }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginVertical: 20, color: '#333' }}>
+        Busca de Filmes
+      </Text>
+      <TextInput
+        style={{ borderWidth: 1, borderRadius: 10, marginVertical: 10, padding: 8, backgroundColor: '#fff' }}
+        placeholder="Digite o nome do filme"
+        value={movieTitle}
+        onChangeText={(text) => setMovieTitle(text)}
+      />
+      <Button title="Buscar Filme" onPress={handleSearch} />
+
+      {location && (
+        <View style={{ marginVertical: 20 }}>
+          <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#333' }}>Sua Localização</Text>
+          <Text>Latitude: {location.coords.latitude}</Text>
+          <Text>Longitude: {location.coords.longitude}</Text>
+          <MapView
+            style={{ width: '100%', height: 200, marginTop: 10 }}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              title="Sua Localização"
+            />
+          </MapView>
+        </View>
+      )}
+
+      {movieData && (
+        <View style={{ marginVertical: 20 }}>
+          <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#333' }}>{movieData.Title}</Text>
+          <Text>Ano: {movieData.Year}</Text>
+          <Text>Gênero: {movieData.Genre}</Text>
+          <Text>Diretor: {movieData.Director}</Text>
+          <Text>Prêmios: {movieData.Awards}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default App;
